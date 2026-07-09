511f3d0be31ba7dcbee3ae8736323952ded9c9c7
CONOR CARTON <cnc125@ic.ac.uk>
2026-07-09 04:58:34 +0100

    Wrote the read_exact() and write_exact() API functions

--- src/socket/socket.cpp
+++ src/socket/socket.cpp
@@ -2,11 +2,12 @@
 
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
+#include <netdb.h>
 #include <unistd.h>
-
-#include <errno.h>
+#include <cerrno>
 #include <cstring>
+#include <stdexcept>
+#include <string>
 
 
 Socket::Socket() {
@@ -17,7 +18,7 @@ Socket::~Socket() {
     close();
 }
 
-//sets up file descriptor and sets to non-blocking returns true if successful
+//sets up file descriptor and returns true if successful
 bool Socket::create() {
     //get file descriptor that uses IPV4 and TCP
     fd_ = ::socket(AF_INET, SOCK_STREAM, 0);
@@ -30,14 +31,13 @@ bool Socket::create() {
 }
 
 //connects over TCP using provideded host and port number
-//registers file descriptor with epoll
 bool Socket::connect(const std::string& host, uint16_t port) {
     sockaddr_in addr;
     //IPV4 address
     addr.sin_family = AF_INET;
     //Sets TCP port in network byte order
     addr.sin_port = htons(port);
-    //Converts host string into binary IPV4 address eg. "192.168.1.10"
+    //Converts host string intoß binary IPV4 address eg. "192.168.1.10"
     inet_pton(AF_INET, host.c_str(), &addr.sin_addr);
 
     int result = ::connect(fd_, (sockaddr *)& addr, sizeof(addr));  
@@ -49,8 +49,40 @@ bool Socket::connect(const std::string& host, uint16_t port) {
     return false;
 }
 
+void Socket::read_exact(uint8_t* dst, size_t len) {
+    size_t received = 0;
 
+    while (received < len) {
+        ssize_t r = read(fd_, dst + received, len - received);
 
+        if (r > 0) {
+            received += r;
+        } else if (r == 0) {
+            throw std::runtime_error("Connection closed");
+        } else {
+            if (errno == EINTR) continue;
+            throw std::runtime_error(strerror(errno));
+        }
+    }
+}
+
+void Socket::write_exact(const uint8_t* src, size_t len) {
+    size_t written = 0;
+
+    while (written < len) {
+        ssize_t r = write(fd_, src + written, len - written);
+
+        if (r > 0) {
+            written += r;
+        } else if (r == 0) {
+            throw std::runtime_error("Write returned zero");
+        } else {
+            if (errno == EINTR) continue;
+            throw std::runtime_error(strerror(errno));
+        }
+
+    }
+}
 
 //closes file descriptor and resets properites of socket object
 void Socket::close() {
@@ -60,4 +92,8 @@ void Socket::close() {
 
     ::close(fd_);
     fd_ = -1;
+}
+
+int Socket::get_fd() {
+    return fd_;
 }
--- src/socket/socket.hpp
+++ src/socket/socket.hpp
@@ -1,39 +1,24 @@
 #pragma once
 
-#include <string>
-#include <vector>
+#include <string>   
 #include <cstdint>
 
 class Socket {
 
 public:
-    enum class State {
-        Uninitialised,
-        Connecting,
-        Connected,
-        Closing,
-        Closed
-    };
 
     //constructor and deconstructor
-    Socket(int epoll_fd);
+    Socket();
     ~Socket();
 
-    //sets up file descriptor and sets to non-blocking returns true if successful
+    //sets up file descriptor and returns true if successful
     bool create();
 
     //connects over TCP using provideded host and port number
-    //registers file descriptor with epoll
     bool connect(const std::string& host, uint16_t port);
 
-    //function handles epoll events by calling approriate write and read helpers
-    bool handle_epoll_event(uint32_t events);
-
-    //public function used by other layers to write data to the output
-    void write_bytes(const std::vector<uint8_t>& data);
-
-    //public function used by other layers to set callback function for reading input stream data
-    void set_read_callback(void (*cb)(const uint8_t*, size_t));
+    void read_exact(uint8_t* dst, size_t len);
+    void write_exact(const uint8_t* src, size_t len);
     
     //returns current state
     State state();
@@ -41,26 +26,8 @@ public:
     //closes file descriptor and resets properites of socket object
     void close();
 
+    int get_fd();
+
 private:
     int fd_;
-    int epoll_fd_;
-    State state_;
-
-    std::vector<uint8_t> outbuf_;
-    std::vector<uint8_t> inbuf_;
-
-    //registers a file descriptor with epoll
-    bool register_with_epoll();
-
-    //helper for reading from input steam data
-    bool pump_read();
-
-    //helper for writing to output steam
-    bool pump_write();
-
-    //updates the events that epoll is monitoring
-    void update_epoll_interest(uint32_t new_events);
-
-    void (*read_callback_)(const uint8_t*, size_t);
-
 };
